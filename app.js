2e8030f00c53a40261a0f9c68439063fef430cc3
YourName <noor11809027@gmail.com>
2022-08-31 11:55:18 +0600

    dynamic CSS class

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const app = Vue.createApp({
   },
   methods: {
     boxSelected(box) {
-        console.log( this.boxASelected)
-        console.log( this.boxBSelected)
-        console.log( this.boxCSelected)
-      if (box === 'A') {
-        this.boxASelected = true;
-        console.log("A", this.boxASelected)
-      } else if (box === 'B') {
-        this.boxBSelected = true;
-        console.log("B", this.boxBSelected)
-      } else if (box === 'C') {
-        this.boxCSelected = true;
-        console.log("C", this.boxCSelected)
+      console.log(this.boxASelected);
+      console.log(this.boxBSelected);
+      console.log(this.boxCSelected);
+      if (box === "A") {
+        this.boxASelected = !this.boxASelected;
+        console.log("A", this.boxASelected);
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;;
+        console.log("B", this.boxBSelected);
+      } else if (box === "C") {
+        this.boxCSelected = !this.boxCSelected;;
+        console.log("C", this.boxCSelected);
       }
     },
   },
